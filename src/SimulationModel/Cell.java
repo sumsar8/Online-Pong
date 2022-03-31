c33c61b8a1e58408d6663c4fb451e73a8ae95a9c
sumsar8 <60388964+sumsar8@users.noreply.github.com>
2022-03-31 12:51:10 +0200

    Initial commit

---
+++ src/SimulationModel/Cell.java
@@ -0,0 +1,33 @@
+package SimulationModel;
+
+import Shapes.Point;
+import Shapes.Shape;
+import Shapes.Sprite;
+
+/**
+ * This is a class
+ * Created 2021-11-15
+ *
+ * @author Magnus Silverdal
+ */
+public class Cell {
+    private int x;
+    private int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Shape getShape() {
+        return new Point(x,y);
+    }
+
+    public void update() {
+
+    }
+
+    public Sprite getSprite() {
+        return null;
+    }
+}
